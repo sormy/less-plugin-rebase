526503934e21e01c043a6da93dd23f0cb9b09eb7
Artem Petrov <saint.tema@gmail.com>
2016-02-29 12:00:09 -0600

    Added rebase-css-processor.js

---
+++ rebase-css-processor.js
@@ -0,0 +1,22 @@
+var rebaseUrls = require('./node_modules/clean-css/lib/urls/rebase');
+
+function RebaseCssProcessor(options) {
+    this.context = {
+        warnings: [],
+        options: {
+            explicitRoot: !!options.root,
+            explicitTarget: !!options.target,
+            relativeTo: options.relativeTo,
+            root: options.root || process.cwd(),
+            target: options.target
+        }
+    };
+};
+
+RebaseCssProcessor.prototype = {
+    process: function (css, extra) {
+        return rebaseUrls(css, this.context);
+    }
+};
+
+module.exports = RebaseCssProcessor;
